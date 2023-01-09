d308c8abc1eb5bfe1049ed9bd7874f456b216405
Ayinla Kwamdeen <ayinlakwamdeen@Ayinlas-MacBook-Pro.local>
2023-01-09 23:43:50 +0000

    Included environment variables

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -27,8 +29,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Hellothisisasecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("user",userSchema);
