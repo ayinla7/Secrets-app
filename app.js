ba65f8809df94663548b9736a16b71d2ae955b4e
Ayinla Kwamdeen <ayinlakwamdeen@Ayinlas-MacBook-Pro.local>
2023-01-09 23:59:38 +0000

    Included Hashing

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -29,7 +30,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+//mongoose encryption
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model("user",userSchema);
@@ -50,7 +52,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -64,7 +66,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username}, function(err, foundUser){
     if(err){
