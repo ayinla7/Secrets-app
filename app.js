4f55a6960bf4e1bedf9ad3c12f374ee9d93514ce
Ayinla Kwamdeen <ayinlakwamdeen@Ayinlas-MacBook-Pro.local>
2023-01-09 22:50:42 +0000

    second  Commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -13,11 +14,18 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("user",userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
 });
-
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -28,8 +36,39 @@ app.get("/register", function(req, res){
 });
 
 
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
 
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
 
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }else{
+          console.log("Wrong password")
+        }
+      }
+    }
+  });
+});
 
 
 
